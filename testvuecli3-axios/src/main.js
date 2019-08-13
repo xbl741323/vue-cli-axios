cc84711ccd333f948d1db9cf379e8a60a24e4ea9
xbl <1546766776@qq.com>
2019-08-13 18:33:39 +0800

    打卡

--- testvuecli3-axios/src/main.js
+++ testvuecli3-axios/src/main.js
@@ -3,14 +3,14 @@ import App from './App.vue'
 import axios from 'axios'//导入axios
 
 Vue.config.productionTip = false
-
+axios.defaults.baseURL = 'http://localhost:3300'
 new Vue({
   render: h => h(App),
 }).$mount('#app')
 
 //axios基本用法
 axios({
-  url: 'http://localhost:3300/getProduct',
+  url: '/getProduct',
   method: 'GET'
 }).then(result => {
   console.log(result.data)
@@ -18,7 +18,7 @@ axios({
 
 //axios传参
 axios({
-  url: 'http://localhost:3300/getProductById',
+  url: '/getProductById',
   params: {
     id: 1
   },
@@ -29,29 +29,49 @@ axios({
 
 //axios发送并发请求
 axios.all([axios({
-url:'http://localhost:3300/getProduct',
-method:'get'
-}),axios({
-  url:'http://localhost:3300/getProductById',
-  params:{
-    id:2
+  url: '/getProduct',
+  method: 'get'
+}), axios({
+  url: '/getProductById',
+  params: {
+    id: 2
   },
-  method:'get'
+  method: 'get'
 })]).then(results => {
   console.log(results)
 })
 
 //调用axios中的spread方法分割数组数据
 axios.all([axios({
-  url:'http://localhost:3300/getProduct',
-  method:'get'
-  }),axios({
-    url:'http://localhost:3300/getProductById',
-    params:{
-      id:2
-    },
-    method:'get'
-  })]).then(axios.spread((res1,res2) => {
-    console.log(res1)
-    console.log(res2)
-  }))
+  url: '/getProduct',
+  method: 'get'
+}), axios({
+  url: '/getProductById',
+  params: {
+    id: 2
+  },
+  method: 'get'
+})]).then(axios.spread((res1, res2) => {
+  console.log(res1)
+  console.log(res2)
+}))
+
+//创建对应的axios的实例
+const instance01 = axios.create({
+  baseURL: 'http://localhost:3300'
+})
+
+instance01({
+  url: '/getProduct'
+}).then(res => {
+  console.log(res.data.message)
+})
+
+instance01({
+  url: '/getProductById',
+  params: {
+    id: 3
+  }
+}).then(res => {
+  console.log(res.data.message)
+})
